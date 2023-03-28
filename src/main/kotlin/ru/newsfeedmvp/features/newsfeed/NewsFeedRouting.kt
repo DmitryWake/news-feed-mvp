b0e1c793e3b0cd67c90485a417ff7a33a99e2b9e
Dmitry Wake <nikolaevskiy.work@gmail.com>
2023-03-28 22:43:22 +0300

    fix newsfeed

--- src/main/kotlin/ru/newsfeedmvp/features/newsfeed/NewsFeedRouting.kt
+++ src/main/kotlin/ru/newsfeedmvp/features/newsfeed/NewsFeedRouting.kt
@@ -3,10 +3,8 @@ package ru.newsfeedmvp.features.newsfeed
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import ru.newsfeedmvp.features.newsfeed.model.FeedInputParamsDTO
 import ru.newsfeedmvp.plugins.AUTH_TYPE_BEARER
 import ru.newsfeedmvp.plugins.checkUserId
 
@@ -16,15 +14,17 @@ fun Routing.configureNewsFeedRouting() {
             kotlin.runCatching { call.request.checkUserId() }
                 .onSuccess {
                     val request = kotlin.runCatching {
-                        call.receive<FeedInputParamsDTO>()
+                        with(call.request.queryParameters) {
+                            get("count")?.toInt() to get("offset")?.toLong()
+                        }
                     }.onFailure { println(it.message) }.getOrNull()
 
                     if (request != null) {
                         kotlin.runCatching {
                             NewsFeedInteractor.instance.getNewsFeed(
                                 it.id,
-                                request.count,
-                                request.offset
+                                request.first!!,
+                                request.second!!
                             )
                         }.onSuccess {
                             call.respond(HttpStatusCode.OK, it)
